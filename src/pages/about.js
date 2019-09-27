f3e18ac3cd63bae986b34403ab50ae89654257cc
arhoy <arhoy@ualberta.ca>
2019-09-27 11:50:55 -0600

    first commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const about = () => {
+    return (
+        <div>
+            <h3> About Gatsby</h3>
+            This is the about page
+        </div>
+    )
+}
+
+export default about
